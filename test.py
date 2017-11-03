443bf7b8298d52776fe3d8ba3728b7df7d676c72
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-11-03 11:21:57 -0400

    add a first round AWS test

--- test.py
+++ test.py
@@ -2,11 +2,12 @@ from __future__ import print_function
 import unittest
 import datetime
 import json
+import app
 from chalicelib.checksuite import CheckSuite
 from chalicelib.ff_connection import FFConnection
 
 
-class TestCheckSuite(unittest.TestCase):
+class TestUnitTests(unittest.TestCase):
     connection = FFConnection('test', None, None, None)
     suite = CheckSuite(connection)
 
@@ -20,7 +21,7 @@ class TestCheckSuite(unittest.TestCase):
         self.assertTrue(check_res.get('status') == 'FAIL')
         self.assertTrue(check_res.get('name') == 'status_of_servers')
 
-    def test_checkresult(self):
+    def test_checkresult_basics(self):
         test_check = self.suite.init_check('test_check', description='Unittest check')
         self.assertTrue(test_check.s3connection.status_code == 404)
         self.assertTrue(test_check.get_latest_check() is None)
@@ -35,6 +36,18 @@ class TestCheckSuite(unittest.TestCase):
         self.assertTrue(check_res.get('name') == formatted_res.get('name'))
         self.assertTrue(check_res.get('description') == "Malformed status; look at Foursight check definition.")
         self.assertTrue(check_res.get('brief_output') == formatted_res.get('brief_output') == None)
+
+
+class TestIntegrated(unittest.TestCase):
+    self.environ = 'webprod' # hopefully this is up
+    self.environ2 = 'webdev' # back up if self.environ is down
+
+    def test_init_connection(self):
+        conn, _ = app.init_connection(self.environ)
+        if conn is None:
+            conn, _ = app.init_connection(self.environ2)
+        self.assertFalse(conn is None)
+        self.assertTrue(conn.is_up)
 
 
 if __name__ == '__main__':
