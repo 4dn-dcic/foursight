3da4f1562623ddf7a5b4c99dd5863f9c5cd7c5ec
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-12-13 16:21:51 -0500

    Added uuid support to put_check

--- app.py
+++ app.py
@@ -291,7 +291,9 @@ def put_check(environ, check):
     """
     Take a PUT request. Body of the request should be a json object with keys
     corresponding to the fields in CheckResult, namely:
-    title, status, description, brief_output, full_output.
+    title, status, description, brief_output, full_output, uuid.
+    If uuid is provided and a previous check is found, the default
+    behavior is to append brief_output and full_output.
     """
     connection, response = init_response(environ)
     if not connection:
@@ -308,12 +310,24 @@ def put_check(environ, check):
         }
         response.status_code = 400
         return response
-    putCheck = CheckResult(connection.s3_connection, check)
+    put_uuid = put_data.get('uuid')
+    putCheck = CheckResult(connection.s3_connection, check, uuid=put_uuid)
     # set valid fields from the PUT body. should this be dynamic?
     # if status is not included, it will be set to ERROR
     for field in ['title', 'status', 'description', 'brief_output', 'full_output']:
         put_content = put_data.get(field)
+        prev_content = getattr(putCheck, field, None)
         if put_content:
+            # append attribute data for _output fields if there are pre-existing
+            # values originating from an existing put_uuid
+            if prev_content and field in ['full_output', 'brief_output']:
+                # will be list, dict, or string. make sure they are same type
+                if isinstance(prev_content, dict) and isinstance(put_content, dict):
+                    put_content = prev_content.update(put_content)
+                elif isinstance(prev_content, list) and isinstance(put_content, list):
+                    put_content = prev_content.append(put_content)
+                elif isinstance(prev_content, unicode) and isinstance(put_content, unicode):
+                    put_content = prev_content + put_content
             setattr(putCheck, field, put_content)
     stored = putCheck.store_result()
     response.body = {
