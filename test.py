d3a230f08693b1fc845805116fa119dbd425b03b
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-01-26 09:59:22 -0500

    Improvement to test_get_foursight_history

--- test.py
+++ test.py
@@ -125,7 +125,17 @@ class TestAppRoutes(FSTest):
         self.assertTrue(len(history[0]) == 2)
         self.assertTrue(isinstance(history[0][0], app_utils.basestring))
         self.assertTrue(isinstance(history[0][1], dict))
+        self.assertTrue('uuid' in history[0][1])
+        self.assertTrue('primary' in history[0][1])
+        first_uuid_1 = history[0][1]['uuid']
+        second_uuid_1 = history[1][1]['uuid']
         self.assertTrue(len(history) == 3)
+        # different start and limit
+        history = app_utils.get_foursight_history(self.conn, test_check, 1, 4)
+        first_uuid_2 = history[0][1]['uuid']
+        self.assertTrue(first_uuid_1 != first_uuid_2)
+        self.assertTrue(second_uuid_1 == first_uuid_2)
+        self.assertTrue(len(history) == 4)
         # bad check
         bad_history = app_utils.get_foursight_history(self.conn, 'not_a_real_check', 0, 3)
         self.assertTrue(bad_history == [])
