69d89a0c805ac2f552a51242ec322179e565b9d2
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-04-04 13:34:41 -0400

    Simplified indexing_records check

--- chalicelib/system_checks.py
+++ chalicelib/system_checks.py
@@ -175,40 +175,12 @@ def indexing_records(connection, **kwargs):
         time_diff = (datetime.datetime.utcnow() -
             datetime.datetime.strptime(rec['_id'], "%Y-%m-%dT%H:%M:%S.%f"))
         if time_diff < delta_days:
-            body = rec['_source'].get('indexing_record')
-            if not body:
-                this_record = {
-                    'timestamp': rec['_id'],
-                    'to_index': rec['_source'].get('to_index'),
-                    'finished': False
-                }
-                warn_records.append(this_record)
-                if check.status == 'IGNORE': check.status = 'WARN'
-            else:
-                this_record = {'record': body}
-                elapsed = body.get('indexing_elapsed')
-                indexed = body.get('indexed')
-                if elapsed and indexed:
-                    if 'day' in elapsed:
-                        elapsed_dt = datetime.datetime.strptime(elapsed, "%d day, %H:%M:%S.%f")
-                        # required to make the days to effect
-                        base_dt = datetime.datetime(1899, 12, 31)
-                    else:
-                        elapsed_dt = datetime.datetime.strptime(elapsed, "%H:%M:%S.%f")
-                        base_dt = datetime.datetime(1900, 1, 1)
-                    elapsed_mins = ((elapsed_dt-base_dt).total_seconds() / 60.0)
-                    this_record['indexed_per_minute'] = indexed / elapsed_mins
-                if body.get('last_xmin') is None or body.get('types_indexed') == 'all':
-                    this_record['complete_index'] = True
-                else:
-                    this_record['complete_index'] = False
-                this_record['indexed'] = indexed
-                this_record['timestamp'] = rec['_id']
-                this_record['finished'] = True
-                if body.get('errors'):
-                    warn_records.append(this_record)
-                    check.status = 'FAIL'
-            recent_records.append(this_record)
+            body = rec['_source']
+            # needed to handle transition to queue. can use 'indexing_started'
+            body['timestamp'] = rec['_id']
+            if body.get('errors') or body.get('indexing_status') != 'finished':
+                warn_records.append(body)
+            recent_records.append(body)
     del all_records
     # sort so most recent records are first
     sort_records = sorted(recent_records, key=lambda rec: datetime.datetime.strptime(rec['timestamp'], "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
@@ -216,6 +188,7 @@ def indexing_records(connection, **kwargs):
     if warn_records:
         sort_warn_records = sorted(warn_records, key=lambda rec: datetime.datetime.strptime(rec['timestamp'], "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
         check.description = 'One or more indexing runs in the past three days may require attention.'
+        check.status = 'WARN'
         check.brief_output = sort_warn_records
     else:
         check.description = 'Indexing runs from the past three days seem normal.'
