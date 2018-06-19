69b9cd85ec1b881749cfa11d23cd6062506fe211
j1z0 <jeremy@softworks.com.my>
2018-06-19 16:26:34 -0400

    seperate backup repository for each env

--- chalicelib/checks/system_checks.py
+++ chalicelib/checks/system_checks.py
@@ -17,6 +17,7 @@ import json
 import datetime
 import boto3
 import time
+import curator
 
 
 @check_function()
@@ -74,15 +75,14 @@ def elastic_beanstalk_health(connection, **kwargs):
         full_output['instance_health'].append(inst_info)
     if full_output['color'] == 'Grey':
         check.status = 'WARN'
-        check.summary = check.description = 'EB environment is updating'
+        check.description = 'EB environment is updating.'
     elif full_output['color'] == 'Yellow':
         check.status = 'WARN'
-        check.summary = check.description = 'EB environment is compromised; requests may fail'
+        check.description = 'EB environment is compromised; requests may fail.'
     elif full_output['color'] == 'Red':
         check.status = 'FAIL'
-        check.summary = check.description = 'EB environment is degraded; requests are likely to fail'
+        check.description = 'EB environment is degraded; requests are likely to fail.'
     else:
-        check.summary = check.description = 'EB environment seems healthy'
         check.status = 'PASS'
     check.full_output = full_output
     return check
@@ -112,16 +112,13 @@ def status_of_elasticsearch_indices(connection, **kwargs):
     # set fields, store result
     if not index_info:
         check.status = 'FAIL'
-        check.summary = 'Error reading status of ES indices'
-        check.description = 'Error reading status of ES indices'
+        check.description = 'Error reading status of ES.'
     elif warn_index_info:
         check.status = 'WARN'
-        check.summary = 'ES indices may not be healthy'
         check.description = 'One or more ES indices have health != green or status != open.'
         check.brief_output = warn_index_info
     else:
         check.status = 'PASS'
-        check.summary = 'ES indices seem healthy'
     check.full_output = index_info
     return check
 
@@ -142,19 +139,16 @@ def indexing_progress(connection, **kwargs):
     diff_unindexed = latest_unindexed - prior_unindexed
     if diff_unindexed == 0 and latest_unindexed != 0:
         check.status = 'FAIL'
-        check.summary = 'Indexing is not progressing'
         check.description = ' '.join(['Total number of unindexed items is',
             str(latest_unindexed), 'and has not changed in the past thirty minutes.',
             'The indexer may be malfunctioning.'])
     elif diff_unindexed > 0:
-        check.status = 'PASS'
-        check.summary = 'Indexing load has increased'
+        check.status = 'WARN'
         check.description = ' '.join(['Total number of unindexed items has increased by',
             str(diff_unindexed), 'in the past thirty minutes. Remaining items to index:',
             str(latest_unindexed)])
     else:
         check.status = 'PASS'
-        check.summary = 'Indexing seems healthy'
         check.description = ' '.join(['Indexing seems healthy. There are', str(latest_unindexed),
         'remaining items to index, a change of', str(diff_unindexed), 'from thirty minutes ago.'])
     return check
@@ -192,11 +186,11 @@ def indexing_records(connection, **kwargs):
     check.full_output = sort_records
     if warn_records:
         sort_warn_records = sorted(warn_records, key=lambda rec: datetime.datetime.strptime(rec['timestamp'], "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
-        check.summary = check.description = 'Indexing runs in the past three days may require attention'
+        check.description = 'One or more indexing runs in the past three days may require attention.'
         check.status = 'WARN'
         check.brief_output = sort_warn_records
     else:
-        check.summary = check.description = 'Indexing runs from the past three days seem normal'
+        check.description = 'Indexing runs from the past three days seem normal.'
         check.status = 'PASS'
     return check
 
@@ -268,10 +262,10 @@ def fourfront_performance_metrics(connection, **kwargs):
 
 @check_function()
 def secondary_queue_deduplication(connection, **kwargs):
-    from ..utils import get_stage_info
+    from .app_utils import STAGE
     check = init_check_res(connection, 'secondary_queue_deduplication')
     # maybe handle this in check_setup.json
-    if get_stage_info()['stage'] != 'prod':
+    if STAGE != 'prod':
         check.full_output = 'Will not run on dev foursight.'
         check.status = 'PASS'
         return check
@@ -374,11 +368,9 @@ def secondary_queue_deduplication(connection, **kwargs):
     }
     if failed:
         check.status = 'WARN'
-        check.summary = 'Queue deduplication encountered an error'
         check.full_output['failed'] = failed
     else:
         check.status = 'PASS'
-        check.summary = 'Removed %s duplicates from %s secondary queue' % (deduplicated, connection.ff_env)
     check.description = 'Items on %s secondary queue were deduplicated. Started with approximately %s items; replaced %s items and removed %s duplicates. Covered %s unique uuids. Took %s seconds.' % (connection.ff_env, starting_count, replaced, deduplicated, len(seen_uuids), elapsed)
 
     return check
@@ -390,22 +382,18 @@ def clean_up_travis_queues(connection, **kwargs):
     Clean up old sqs queues based on the name ("travis-job")
     and the creation date. Only run on data for now
     """
-    from ..utils import get_stage_info
+    from .utils import STAGE
     check = init_check_res(connection, 'clean_up_travis_queues')
     check.status = 'PASS'
-    if connection.fs_env != 'data' or get_stage_info()['stage'] != 'prod':
-        check.summary = check.description = 'This check only runs on the data environment for Foursight prod'
+    if connection.fs_env != 'data' or STAGE != 'prod':
+        check.description = 'This check only runs on the data environment for Foursight prod.'
         return check
-    sqs_client = boto3.client('sqs')
     sqs = boto3.resource('sqs')
     queues = sqs.queues.all()
     num_deleted = 0
     for queue in queues:
         if 'travis-job' in queue.url:
-            try:
-                creation = queue.attributes['CreatedTimestamp']
-            except sqs_client.exceptions.QueueDoesNotExist:
-                continue
+            creation = queue.attributes['CreatedTimestamp']
             if isinstance(creation, basestring):
                 creation = float(creation)
             dt_creation = datetime.datetime.utcfromtimestamp(creation)
@@ -414,14 +402,13 @@ def clean_up_travis_queues(connection, **kwargs):
             if queue_age > datetime.timedelta(days=3):
                 queue.delete()
                 num_deleted += 1
-    check.summary = 'Cleaned up %s old indexing queues' % num_deleted
+
     check.description = 'Cleaned up all indexing queues from Travis that are 3 days old or older. %s queues deleted.' % num_deleted
     return check
 
 
 @check_function()
 def manage_old_filebeat_logs(connection, **kwargs):
-    import curator
     check = init_check_res(connection, 'manage_old_filebeat_logs')
     check.status = "WARNING"
     check.description = "not able to get data from ES"
@@ -432,7 +419,8 @@ def manage_old_filebeat_logs(connection, **kwargs):
     timestring = '%Y.%m.%d'
 
     log_index = 'filebeat-'
-    snapshot = 'backup-'
+    # TODO: this probably needs to change when namespacing is implemented
+    snapshot = 'backup-%s-' % connection.ff_env
     today = datetime.datetime.today().strftime(timestring)
 
     # run the check
@@ -447,12 +435,13 @@ def manage_old_filebeat_logs(connection, **kwargs):
     if len(ilo.indices) > 0:
         try:
             new_snapshot = curator.Snapshot(ilo, repository=snapshot[:-1], name='%s%s' % (snapshot, today))
+            print(new_snapshot.do_dry_run())
             new_snapshot.do_action()
-            check.full_output = "Snapshot taken for %s indices" % len(ilo.indices)
+            check.full_output = "Snapshot taken for %s indicies" % len(ilo.indices)
         except curator.exceptions.FailedExecution as e:
             # snapshot already exists
             if "Invalid snapshot name" in str(e):
-                check.full_output = "Snapshot already exists with same name for %s indices, so skipping." % len(ilo.indices)
+                check.full_output = "Snapshot alread exists with same name for %s indicies. So skipping \n" % len(ilo.indices)
             else:
                 raise(e)
 
@@ -460,10 +449,18 @@ def manage_old_filebeat_logs(connection, **kwargs):
     ilo = es_utils.get_index_list(client, log_index, trim_backup, timestring, ilo=ilo)
 
     if len(ilo.indices) > 0:
-        cleanupIndices = curator.DeleteIndices(ilo)
+        cleanupIndicies = curator.DeleteIndices(ilo)
+        print(cleanupIndicies.do_dry_run())
         cleanupIndices.do_action()
-        check.full_output += " Cleaned up %s old indices" % len(ilo.indices)
+        check.full_output += "\n Cleaned up %s old indicies" % len(ilo.indices)
 
     check.status = "PASS"
-    check.description = 'Performed auto-backup to repository %s' % snapshot[:-1]
+    check.description = 'performed auto-backup to repository %s' % snapshot[:-1]
     return check
+
+'''
+import app
+app.set_timeout(0)
+connection = app.init_connection('mastertest')
+app.run_check_or_action(connection, 'system_checks/manage_old_filebeat_logs', {'called_by':None})
+'''
