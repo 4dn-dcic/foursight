ad2f725a9ca699c0ab9e26281d0d51abed9f3d0b
j1z0 <jeremy@softworks.com.my>
2018-06-19 08:12:23 -0400

    broke out es_utils to dcicutils

--- chalicelib/checks/system_checks.py
+++ chalicelib/checks/system_checks.py
@@ -6,15 +6,17 @@ from ..utils import (
     init_action_res,
     basestring
 )
-from dcicutils import ff_utils
+from dcicutils import (
+    ff_utils,
+    es_utils
+)
+
 import requests
 import sys
 import json
 import datetime
 import boto3
 import time
-import elasticsearch
-import curator
 
 
 @check_function()
@@ -404,52 +406,6 @@ def clean_up_travis_queues(connection, **kwargs):
     return check
 
 
-def get_index_list(client, name, days_old=0, timestring='%Y.%m.%d', ilo=None):
-
-    if ilo is None:
-        ilo = curator.IndexList(client)
-
-    ilo.filter_by_regex(kind='prefix', value=name)
-    ilo.filter_by_age(source='name', direction='older', timestring=timestring, unit='days',
-                      unit_count=days_old)
-    return ilo
-
-
-def create_es_client(es_url, use_aws_auth=False):
-    from elasticsearch import Elasticsearch, RequestsHttpConnection
-    from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
-
-    if isinstance(es_url, (list, tuple)):
-        addresses = es_url
-    else:
-        addresses = [es_url,]
-
-    es_options = {'retry_on_timeout': True,
-                  'maxsize': 50  # parallellism...
-                 }
-    if use_aws_auth:
-        host = addresses[0].split('//')
-        host = host[-1].split(":")
-        auth = BotoAWSRequestsAuth(aws_host=host[0].rstrip('/'),
-                                   aws_region='us-east-1',
-                                   aws_service='es')
-        es_options['connection_class'] = RequestsHttpConnection
-        es_options['http_auth'] = auth
-
-    return Elasticsearch(addresses, **es_options)
-
-
-def create_snapshot_repo(client, s3_bucket, repo_name='backup'):
-    snapshot_body = {'type': 's3',
-                     'settings': {
-                         'bucket': s3_bucket,
-                         'region': 'us-east-1',
-                         'role_arn': 'arn:aws:iam::643366669028:role/S3Roll'
-                     }
-                    }
-    res = client.snapshot.create_repository(repository=repo_name, body=snapshot_body)
-
-
 @check_function()
 def manage_old_filebeat_logs(connection, **kwargs):
     check = init_check_res(connection, 'manage_old_filebeat_logs')
@@ -466,11 +422,13 @@ def manage_old_filebeat_logs(connection, **kwargs):
     today = datetime.datetime.today().strftime(timestring)
 
     # run the check
-    client = create_es_client(connection.ff_es, True)
-    create_snapshot_repo(client, 'foursight-runs', snapshot[:-1])
+    client = es_utils.create_es_client(connection.ff_es, True)
+    # store backups in foursight s3, cause I know we have access to this..
+    # maybe this should change?
+    es_utils.create_snapshot_repo(client, 'foursight-runs', snapshot[:-1])
 
     # amazon es auto backups first 14 days, so we only need backup after that
-    ilo = get_index_list(client, log_index, start_backup, timestring)
+    ilo = es_utils.get_index_list(client, log_index, start_backup, timestring)
 
     import pdb; pdb.set_trace()
     if len(ilo.indices) > 0:
@@ -487,7 +445,7 @@ def manage_old_filebeat_logs(connection, **kwargs):
                 raise(e)
 
     # now trim further to only be indexes 30-days or older and delete
-    ilo = get_index_list(client, log_index, trim_backup, timestring, ilo=ilo)
+    ilo = es_utils.get_index_list(client, log_index, trim_backup, timestring, ilo=ilo)
 
     if len(ilo.indices) > 0:
         cleanupIndicies = curator.DeleteIndices(ilo)
