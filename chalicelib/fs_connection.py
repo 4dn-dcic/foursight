23572218c9604d162fd3b6227bd5227bac4b739d
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-08-21 16:42:04 -0400

    Revert "Allow local env in fs_connection"
    This reverts commit a627727c76cb86cedaefaaaed19a4231a847bd5a.

--- chalicelib/fs_connection.py
+++ chalicelib/fs_connection.py
@@ -15,19 +15,19 @@ class FSConnection(object):
     - ff_keys: FF keys for the environment with 'key', 'secret' and 'server'
     - ff_es: string server of the elasticsearch for the FF
     - s3_connection: S3Connection object that is the s3 connection for FS
-
+    
     If param test=True, then do not actually attempt to initate the FF connections
     """
     def __init__(self, fs_environ, fs_environ_info, test=False):
         # FOURSIGHT information
         self.fs_env = fs_environ
         self.s3_connection = S3Connection(fs_environ_info.get('bucket'))
-
+        
         # FOURFRONT information
         self.ff_server = fs_environ_info.get('fourfront')
         self.ff_env = fs_environ_info.get('ff_env')
         self.ff_es = fs_environ_info.get('es')
-        if self.fs_env != 'local' or not test:
+        if not test:
             self.ff_s3 = s3Utils(env=self.ff_env)
             try:
                 self.ff_keys = self.ff_s3.get_access_keys()
