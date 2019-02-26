a6cd141d49b4c0f4e7e77b66a61e4c076410f4e5
Sarah <sarah_reiff@hms.harvard.edu>
2019-02-26 13:12:42 -0500

    paired_end_info_consistent moved to audit_checks, badge action removed

--- chalicelib/checks/audit_checks.py
+++ chalicelib/checks/audit_checks.py
@@ -170,6 +170,38 @@ def expset_opf_unique_files_in_experiments(connection, **kwargs):
 
 
 @check_function()
+def paired_end_info_consistent(connection, **kwargs):
+    # check that fastqs with a paired_end number have a paired_with related_file, and vice versa
+    check = init_check_res(connection, 'paired_end_info_consistent')
+
+    search1 = 'search/?type=FileFastq&related_files.relationship_type=paired+with&paired_end=No+value'
+    search2 = 'search/?type=FileFastq&related_files.relationship_type!=paired+with&paired_end%21=No+value'
+
+    results1 = ff_utils.search_metadata(search1 + '&frame=object', ff_env=connection.ff_env)
+    results2 = ff_utils.search_metadata(search2 + '&frame=object', ff_env=connection.ff_env)
+
+    results = {'paired with file missing paired_end number':
+               [result1['@id'] for result1 in results1],
+               'file with paired_end number missing "paired with" related_file':
+               [result2['@id'] for result2 in results2]}
+
+    if [val for val in results.values() if val]:
+        check.status = 'WARN'
+        check.summary = 'Inconsistencies found in FileFastq paired end info'
+        check.description = ('{} files found with a "paired with" related_file but missing a paired_end number; '
+                             '{} files found with a paired_end number but missing related_file info'
+                             ''.format(len(results['paired with file missing paired_end number']),
+                                       len(results['file with paired_end number missing "paired with" related_file'])))
+    else:
+        check.status = 'PASS'
+        check.summary = 'No inconsistencies in FileFastq paired end info'
+        check.description = 'All paired end fastq files have both paired end number and "paired with" related_file'
+    check.full_output = results
+    check.brief_output = [item for val in results.values() for item in val]
+    return check
+
+
+@check_function()
 def workflow_properties(connection, **kwargs):
     check = init_check_res(connection, 'workflow_properties')
 
