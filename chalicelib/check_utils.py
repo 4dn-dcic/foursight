ab937abea77a8eeb1d914c2a56964bc47fe435f1
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-04-18 10:38:26 -0400

    Quick fix to order checks on UI by title

--- chalicelib/check_utils.py
+++ chalicelib/check_utils.py
@@ -207,7 +207,7 @@ def get_check_schedule(schedule_name):
 def get_check_results(connection, checks=[], use_latest=False):
     """
     Initialize check results for each desired check and get results stored
-    in s3, sorted by status and then alphabetically.
+    in s3, sorted by status and then alphabetically by title.
     May provide a list of string check names as `checks`; otherwise get all
     checks by default.
     By default, gets the 'primary' results. If use_latest is True, get the
@@ -225,9 +225,12 @@ def get_check_results(connection, checks=[], use_latest=False):
         # checks with no records will return None. Skip IGNORE checks
         if found and found.get('status') != 'IGNORE':
             check_results.append(found)
-    # sort them by status and alphabetically by name
+    # sort them by status and then alphabetically by check_setup title
     stat_order = ['ERROR', 'FAIL', 'WARN', 'PASS']
-    return sorted(check_results, key=lambda v: (stat_order.index(v['status']) if v['status'] in stat_order else 9, v['name'].lower()))
+    return sorted(
+        check_results,
+        key=lambda v: (stat_order.index(v['status']) if v['status'] in stat_order else 9, get_check_title_from_setup(v['name']).lower())
+    )
 
 
 def get_grouped_check_results(connection):
