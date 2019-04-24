a27ebb3d5534009c5f7f831ad052a0f303d05503
Koray Kırlı <koray_kirli@hms.harvard.edu>
2019-04-24 14:37:03 -0400

    chip seq

--- chalicelib/checks/wfr_checks.py
+++ chalicelib/checks/wfr_checks.py
@@ -1037,7 +1037,7 @@ def tsa_seq_status(connection, **kwargs):
     check.summary = ""
     check.full_output = {'skipped': [], 'running_runs': [], 'needs_runs': [], 'completed_runs': [], 'problematic_runs':[]}
     check.status = 'PASS'
-    exp_type = 'TSA-seq''
+    exp_type = 'TSA-seq'
     return check
 
 
@@ -1076,7 +1076,7 @@ def nad_seq_status(connection, **kwargs):
     check.summary = ""
     check.full_output = {'skipped': [], 'running_runs': [], 'needs_runs': [], 'completed_runs': [], 'problematic_runs':[]}
     check.status = 'PASS'
-    exp_type = 'NAD-seq''
+    exp_type = 'NAD-seq'
     return check
 
 
@@ -1115,7 +1115,7 @@ def atac_seq_status(connection, **kwargs):
     check.summary = ""
     check.full_output = {'skipped': [], 'running_runs': [], 'needs_runs': [], 'completed_runs': [], 'problematic_runs':[]}
     check.status = 'PASS'
-    exp_type = 'ATAC-seq''
+    exp_type = 'ATAC-seq'
     return check
 
 
@@ -1124,6 +1124,45 @@ def atac_seq_start(connection, **kwargs):
     """Start runs by sending compiled input_json to run_workflow endpoint"""
     start = datetime.utcnow()
     action = init_action_res(connection, 'atac_seq_start')
+    my_auth = connection.ff_keys
+    my_env = connection.ff_env
+    check_result = action.get_associated_check_result(kwargs).get('full_output', {})
+    missing_runs = []
+    patch_meta = []
+    if kwargs.get('start_runs'):
+        missing_runs = check_result.get('needs_runs')
+    if kwargs.get('patch_completed'):
+        patch_meta = check_result.get('completed_runs')
+    action = wfr_utils.start_hic_tasks(missing_runs, patch_meta, action, my_auth, my_env, start, move_to_pc=False)
+    return action
+
+
+@check_function(lab_title=None, start_date=None)
+def chip_seq_status(connection, **kwargs):
+    """
+    Keyword arguments:
+    lab_title -- limit search with a lab i.e. Bing+Ren, UCSD
+    start_date -- limit search to files generated since a date formatted YYYY-MM-DD
+    run_time -- assume runs beyond run_time are dead
+    """
+    start = datetime.utcnow()
+    check = init_check_res(connection, 'chip_seq_status')
+    my_auth = connection.ff_keys
+    check.action = "chip_seq_start"
+    check.description = "run missing steps and add processing results to processed files, match set status"
+    check.brief_output = ['All Good!']
+    check.summary = ""
+    check.full_output = {'skipped': [], 'running_runs': [], 'needs_runs': [], 'completed_runs': [], 'problematic_runs':[]}
+    check.status = 'PASS'
+    exp_type = 'ChIP-seq'
+    return check
+
+
+@action_function(start_runs=False, patch_completed=False)
+def chip_seq_start(connection, **kwargs):
+    """Start runs by sending compiled input_json to run_workflow endpoint"""
+    start = datetime.utcnow()
+    action = init_action_res(connection, 'chip_seq_start')
     my_auth = connection.ff_keys
     my_env = connection.ff_env
     check_result = action.get_associated_check_result(kwargs).get('full_output', {})
