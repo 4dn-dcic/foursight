c337789360b0876dd33aa6203748a049395e3326
Koray Kırlı <koray_kirli@hms.harvard.edu>
2019-04-29 13:29:23 -0400

    retry without spot

--- chalicelib/checks/helpers/wfrset_utils.py
+++ chalicelib/checks/helpers/wfrset_utils.py
@@ -150,7 +150,8 @@ def step_settings(step_name, my_organism, attribution, overwrite=None):
         "spot_instance": True,
         "ebs_iops": "",
         "log_bucket": "tibanna-output",
-        "key_name": "4dn-encode"
+        "key_name": "4dn-encode",
+        "behavior_on_capacity_limit": "retry_without_spot"
         }
     if not template.get('parameters'):
         template['parameters'] = {}
