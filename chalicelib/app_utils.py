b5dded7da6a8240a038f3b37ab0b54c109dca846
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-08-28 16:54:32 -0400

    Handle all_4dn environment for schedules and fix test

--- chalicelib/app_utils.py
+++ chalicelib/app_utils.py
@@ -753,10 +753,13 @@ def queue_scheduled_checks(sched_environ, schedule_name, conditions=None):
     """
     queue = get_sqs_queue()
     if schedule_name is not None:
-        if sched_environ != 'all' and sched_environ not in list_environments():
+        if sched_environ not in ['all', 'all_4dn'] and sched_environ not in list_environments():
             print('-RUN-> %s is not a valid environment. Cannot queue.' % sched_environ)
             return
-        sched_environs = list_environments() if sched_environ == 'all' else [sched_environ]
+        sched_environs = list_environments() if sched_environ in ['all', 'all_4dn'] else [sched_environ]
+        # remove any environments with 'cgap' if all_4dn
+        if sched_environ == 'all_4dn':
+            sched_environs = [env for env in sched_environ if 'cgap' not in env]
         check_schedule = get_check_schedule(schedule_name, conditions)
         if not check_schedule:
             print('-RUN-> %s is not a valid schedule. Cannot queue.' % schedule_name)
@@ -764,6 +767,9 @@ def queue_scheduled_checks(sched_environ, schedule_name, conditions=None):
         for environ in sched_environs:
             # add the run info from 'all' as well as this specific environ
             check_vals = copy.copy(check_schedule.get('all', []))
+            # also add 'all_4dn' if we are in a non-cgap environment
+            if 'cgap' not in environ:
+                check_vals.extend(check_schedule.get('all_4dn', []))
             check_vals.extend(check_schedule.get(environ, []))
             send_sqs_messages(queue, environ, check_vals)
     runner_input = {'sqs_url': queue.url}
