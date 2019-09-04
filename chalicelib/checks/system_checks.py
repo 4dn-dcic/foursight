61aded0611bb37e83af9e6926ee8e7b3b1ad2001
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-09-04 15:18:56 -0400

    Small tweak to indexing_progress to skip query if no records present

--- chalicelib/checks/system_checks.py
+++ chalicelib/checks/system_checks.py
@@ -160,6 +160,13 @@ def indexing_progress(connection, **kwargs):
 def indexing_records(connection, **kwargs):
     check = init_check_res(connection, 'indexing_records')
     client = es_utils.create_es_client(connection.ff_es, True)
+    # make sure we have the index and items within it
+    if (not client.indices.exists('indexing') or
+        client.count('indexing', 'indexing').get('count', 0) < 1):
+        check.summary = check.description = 'No indexing records found'
+        check.status = 'PASS'
+        return check
+
     res = client.search(index='indexing', doc_type='indexing', sort='uuid:desc', size=1000,
                         body={'query': {'query_string': {'query': '_exists_:indexing_status'}}})
     delta_days = datetime.timedelta(days=3)
