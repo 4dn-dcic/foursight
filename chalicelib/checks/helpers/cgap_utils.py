9e65b9ad6cdc0ffc59dc79557ec10b23c80bfa9f
Koray Kırlı <koray_kirli@hms.harvard.edu>
2019-09-08 01:16:40 -0400

    change to tibanna api

--- chalicelib/checks/helpers/cgap_utils.py
+++ chalicelib/checks/helpers/cgap_utils.py
@@ -4,6 +4,8 @@ from operator import itemgetter
 from . import wfrset_cgap_utils
 import json
 
+from tibanna.core import API
+
 lambda_limit = wfrset_cgap_utils.lambda_limit
 
 # check at the end
@@ -307,12 +309,76 @@ def run_missing_wfr(input_json, input_files, run_name, auth, env):
 
     # TEMP
     input_json['step_function_name'] = 'tibanna_zebra'
+
+    # try:
+    #     e = ff_utils.post_metadata(input_json, 'WorkflowRun/run', key=auth)
+    #     url = json.loads(e['input'])['_tibanna']['url']
+    #     return url
+    # except Exception as e:
+    #     return str(e)
+
     try:
-        e = ff_utils.post_metadata(input_json, 'WorkflowRun/run', key=auth)
+        e = API().run_workflow(input_json=input_json, sfn='tibanna_zebra')
         url = json.loads(e['input'])['_tibanna']['url']
         return url
     except Exception as e:
         return str(e)
+
+
+def check_runs_without_output(res, check, run_name, my_auth, start):
+    """Common processing for checks that are running on files and not producing output files
+    like qcs ones producing extra files"""
+    # no successful run
+    missing_run = []
+    # successful run but no expected metadata change (qc or extra file)
+    missing_meta_changes = []
+    # still running
+    running = []
+    # multiple failed runs
+    problems = []
+
+    for a_file in res:
+        # lambda has a time limit (300sec), kill before it is reached so we get some results
+        now = datetime.utcnow()
+        if (now-start).seconds > lambda_limit:
+            check.brief_output.append('did not complete checking all')
+            break
+        file_id = a_file['accession']
+        report = get_wfr_out(a_file, run_name,  key=my_auth, md_qc=True)
+        if report['status'] == 'running':
+            running.append(file_id)
+        elif report['status'].startswith("no complete run, too many"):
+            problems.append(file_id)
+        elif report['status'] != 'complete':
+            missing_run.append(file_id)
+        # There is a successful run, but no extra_file
+        elif report['status'] == 'complete':
+            missing_meta_changes.append(file_id)
+    if running:
+        check.summary = 'Some files are running'
+        check.brief_output.append(str(len(running)) + ' files are still running.')
+        check.full_output['running'] = running
+    if problems:
+        check.summary = 'Some files have problems'
+        check.brief_output.append(str(len(problems)) + ' files have multiple failed runs')
+        check.full_output['problems'] = problems
+        check.status = 'WARN'
+    if missing_run:
+        check.allow_action = True
+        check.summary = 'Some files are missing runs'
+        check.brief_output.append(str(len(missing_run)) + ' files lack a successful run')
+        check.full_output['files_without_run'] = missing_run
+        check.status = 'WARN'
+    if missing_meta_changes:
+        check.allow_action = True
+        check.summary = 'Some files are missing runs'
+        check.brief_output.append(str(len(missing_meta_changes)) + ' files have successful run but no qc/extra file')
+        check.full_output['files_without_changes'] = missing_meta_changes
+        check.status = 'WARN'
+    check.summary = check.summary.strip()
+    if not check.brief_output:
+        check.brief_output = ['All Good!']
+    return check
 
 
 def find_fastq_info(exp, fastq_files):
@@ -331,9 +397,7 @@ def find_fastq_info(exp, fastq_files):
     # check pairing for the first file, and assume all same
     paired = ""
     total_f_size = 0
-    biosample = exp['biosample']
-    organisms = list(set([bs['individual']['organism']['name'] for bs in biosample['biosource']]))
-    organism = organisms[0]
+    organism = 'human'
     exp_files = exp['files']
 
     for fastq_file in exp_files:
