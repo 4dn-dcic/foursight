5a9ff0d9f4d71643182e5aa0a633a2aab3286aad
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2019-10-28 11:26:25 -0400

    fixed is_there_qc_metric function

--- chalicelib/checks/helpers/cgap_utils.py
+++ chalicelib/checks/helpers/cgap_utils.py
@@ -492,12 +492,17 @@ def start_tasks(missing_runs, patch_meta, action, my_auth, my_env, start, move_t
 def is_there_my_qc_metric(file_meta, qc_metric_name, my_auth):
     if not file_meta.get('quality_metric'):
         return False
-    else:
-        qc_results = ff_utils.get_metadata(file_meta['quality_metric']['uuid'], key=my_auth)
+
+    qc_results = ff_utils.get_metadata(file_meta['quality_metric']['uuid'], key=my_auth)
+
+    if qc_results['display_title'].startswith('QualityMetricQclist'):
         if not qc_results.get('qc_list'):
             return False
-        else:
-            for qc in qc_results['qc_list']:
-                if qc_metric_name not in qc['value']['display_title']:
-                    return False
+
+        for qc in qc_results['qc_list']:
+            if qc_metric_name not in qc['value']['display_title']:
+                return False
+    else:
+        if qc_metric_name not in qc_results['display_title']:
+            return False
     return True
