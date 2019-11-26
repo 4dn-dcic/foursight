44870153550ed88855120c58fecf4af854a52c9a
Koray Kırlı <koray_kirli@hms.harvard.edu>
2019-11-26 14:04:40 -0500

    check template

--- chalicelib/checks/wfr_checks.py
+++ chalicelib/checks/wfr_checks.py
@@ -1633,3 +1633,70 @@ def bed2multivec_start(connection, **kwargs):
     action.output = action_logs
     action.status = 'DONE'
     return action
+
+
+@check_function(lab_title=None, start_date=None, query='')
+def rna_seq_status(connection, **kwargs):
+    """
+    Keyword arguments:
+    lab_title -- limit search with a lab i.e. Bing+Ren, UCSD
+    start_date -- limit search to files generated since a date formatted YYYY-MM-DD
+    run_time -- assume runs beyond run_time are dead
+    """
+    start = datetime.utcnow()
+    check = CheckResult(connection, 'rna_seq_status')
+    my_auth = connection.ff_keys
+    check.action = "rna_seq_start"
+    check.description = "run missing steps and add processing results to processed files, match set status"
+    check.brief_output = ['All Good!']
+    check.summary = "All Good!"
+    check.full_output = {'skipped': [], 'running_runs': [], 'needs_runs': [],
+                         'completed_runs': [], 'problematic_runs': []}
+    check.status = 'PASS'
+
+    exp_type = 'RNA-seq'
+    # completion tag
+    tag = wfr_utils.accepted_versions[exp_type][-1]
+
+    # check indexing queue
+    env = connection.ff_env
+    indexing_queue = ff_utils.stuff_in_queues(env, check_secondary=True)
+    if indexing_queue:
+        check.status = 'PASS'  # maybe use warn?
+        check.brief_output = ['Waiting for indexing queue to clear']
+        check.summary = 'Waiting for indexing queue to clear'
+        check.full_output = {}
+        return check
+
+    # Build the query, add date and lab if available
+    user_query = kwargs.get('query')
+    if user_query:
+        query = user_query
+    else:
+        query = wfr_utils.build_exp_type_query(exp_type, kwargs)
+
+    # The search
+    res = ff_utils.search_metadata(query, key=my_auth)
+    if not res:
+        check.summary = 'All Good!'
+        return check
+    check = wfr_utils.check_rna(res, my_auth, tag, check, start, lambda_limit)
+    return check
+
+
+@action_function(start_runs=True, patch_completed=True)
+def rna_seq_start(connection, **kwargs):
+    """Start runs by sending compiled input_json to run_workflow endpoint"""
+    start = datetime.utcnow()
+    action = ActionResult(connection, 'rna_seq_start')
+    my_auth = connection.ff_keys
+    my_env = connection.ff_env
+    check_result = action.get_associated_check_result(kwargs).get('full_output', {})
+    missing_runs = []
+    patch_meta = []
+    if kwargs.get('start_runs'):
+        missing_runs = check_result.get('needs_runs')
+    if kwargs.get('patch_completed'):
+        patch_meta = check_result.get('completed_runs')
+    action = wfr_utils.start_tasks(missing_runs, patch_meta, action, my_auth, my_env, start, move_to_pc=False)
+    return action
