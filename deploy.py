1a7832efc5c529545e8c1e201addfe674e017b9f
William Ronchetti <wrr33@cornell.edu>
2020-01-16 12:23:34 -0500

    up lambda timeout to 15 mins

--- deploy.py
+++ deploy.py
@@ -16,7 +16,7 @@ CONFIG_BASE = {
       "api_gateway_stage": "api",
       "autogen_policy": False,
       "lambda_memory_size": 512,
-      "lambda_timeout": 300,
+      "lambda_timeout": 900,
       "environment_variables": {
           "chalice_stage": "dev"
       }
@@ -25,7 +25,7 @@ CONFIG_BASE = {
       "api_gateway_stage": "api",
       "autogen_policy": False,
       "lambda_memory_size": 512,
-      "lambda_timeout": 300,
+      "lambda_timeout": 900,
       "environment_variables": {
           "chalice_stage": "prod"
       }
