632b0d22aca69ed4f3da663bd5566000f10b2082
William Ronchetti <wrr33@cornell.edu>
2020-05-18 14:50:33 -0400

    C4-58 few more small changes through testing

--- chalicelib/checks/deployment_checks.py
+++ chalicelib/checks/deployment_checks.py
@@ -1,6 +1,6 @@
-import os
-import shutil
+import time
 import boto3
+import shutil
 import datetime
 import tempfile
 from git import Repo
@@ -9,7 +9,7 @@ from ..run_result import CheckResult, ActionResult
 from ..utils import check_function, action_function
 from dcicutils.deployment_utils import EBDeployer
 from dcicutils.env_utils import (
-    FF_ENV_INDEXER, CGAP_ENV_INDEXER, is_indexer_env, is_fourfront_env, is_cgap_env
+    FF_ENV_INDEXER, CGAP_ENV_INDEXER, is_fourfront_env, is_cgap_env
 )
 from dcicutils.beanstalk_utils import beanstalk_info, is_indexing_finished
 
@@ -30,9 +30,9 @@ def clone_repo_to_temporary_dir(repo='https://github.com/4dn-dcic/fourfront.git'
     """ Clones the given repo (default fourfront) to a temporary directory whose
         absolute path is returned.
     """
-    tempdir = tempfile.mkdtemp()
-    Repo.clone_from(url=repo, to_path=tempdir, branch='master')
-    return os.path.join(tempdir, './fourfront')
+    tempdir = tempfile.mkdtemp(prefix=name)
+    Repo.clone_from(url=repo, to_path=tempdir)
+    return tempdir
 
 
 def cleanup_tempdir(tempdir):
@@ -40,44 +40,51 @@ def cleanup_tempdir(tempdir):
     shutil.rmtree(tempdir)
 
 
-@check_function(env=FF_ENV_INDEXER)  # XXX: Scope?
+@check_function(env='fourfront-hotseat')
 def indexer_server_status(connection, **kwargs):
     """ Checks the status of index servers (if any are up) """
     check = CheckResult(connection, 'indexer_server_status')
     check.action = 'terminate_indexer_server'
     env = kwargs.get('env')
+    indexer_env = FF_ENV_INDEXER if is_fourfront_env(env) else CGAP_ENV_INDEXER
+    description = try_to_describe_indexer_env(indexer_env)  # verify an indexer is online
+    if description is None:
+        check.status = 'ERROR'
+        check.summary = 'Did not locate corresponding indexer env: ' \
+                        'given env %s does not have indexer %s' % (env, indexer_env)
+        check.allow_action = False
+        return check
 
-    if not is_indexer_env(env):
-        raise RuntimeError('Bad EB env passed to check, should be one of: %s, %s\n'
-                           'But got: %s' % (FF_ENV_INDEXER, CGAP_ENV_INDEXER, env))
-
-    # Get info about indexer env, check indexing status
-    description = try_to_describe_indexer_env(env)
-    if description is not None:
+    try:
         indexing_finished, counts = is_indexing_finished(env)
-        if indexing_finished is True:
-            check.status = 'PASS'
-            check.summary = 'Env %s has finished indexing and is ready to be terminated' % env
-            check.action_message = 'Will terminate indexer env: %s' % env
-            check.allow_action = True  # allow us to terminate
-        else:
-            check.status = 'WARN'
-            check.allow_action = False
-            check.summary = 'Env %s has not finished indexing with remaining counts %s' % (env, counts)
-    else:
+    except Exception as e:  # XXX: This should be handled in dcicutils -Will 5/18/2020
+        check.status = 'ERROR'
+        check.summary = 'Failed to get indexing status of given env: %s' % str(e)
+        check.allow_action = False
+        return check
+
+    if indexing_finished is True:
         check.status = 'PASS'
-        check.summary = 'Did not find an online indexer with the given name: %s, so no status to check.' % env
+        check.summary = 'Env %s has finished indexing and is ready to be terminated' % env
+        check.action_message = 'Safe to terminate indexer env: %s since indexing is finished' % env
+        check.allow_action = True  # allow us to terminate
+    else:
+        check.status = 'WARN'
+        check.allow_action = True
+        check.action_message = 'Will terminate indexer env %s when indexing is not done yet!' % env
+        check.summary = 'Env %s has not finished indexing with remaining counts %s' % (env, ' '.join(counts))
 
     check.full_output = env  # full_output contains env we checked
     return check
 
 
-@action_function()
+@action_function(forced_env=None)
 def terminate_indexer_server(connection, **kwargs):
     """ Terminates the index_server stored in the full_output member of the associated check above """
     action = ActionResult(connection, 'terminate_indexer_server')
     related_indexer_server_status = action.get_associated_check_result(kwargs)
-    env_to_terminate = related_indexer_server_status.get('full_output', None)
+    forced_env = kwargs.get('forced_env', None)
+    env_to_terminate = related_indexer_server_status.get('full_output', None) if forced_env is None else forced_env
 
     if env_to_terminate:
         client = boto3.client('elasticbeanstalk')
@@ -100,7 +107,8 @@ def provision_indexer_environment(connection, **kwargs):
     """ Provisions an indexer environment for the given env. Note that only one indexer can be online
         per application (one for 4DN, one for CGAP).
 
-        NOTE: env is an EB ENV NAME ('data', 'staging' are NOT valid).
+        IMPORTANT: env is an EB ENV NAME ('data', 'staging' are NOT valid).
+        IMPORTANT: This ecosystem will break some integrated tests in dcicutils
     """
     check = CheckResult(connection, 'provision_indexer_environment')
     env = kwargs.get('env', None)
@@ -164,16 +172,12 @@ def deploy_application_to_beanstalk(connection, **kwargs):
     packaging_was_successful = EBDeployer.build_application_version(repo_location,
                                                                     application_version_name,
                                                                     branch=branch)
+    time.sleep(10)  # give EB some time to index the new template
     if packaging_was_successful:
         try:
-            deploy_succeeded = EBDeployer.deploy_new_version(env, repo_location, application_version_name)
-            if deploy_succeeded:
-                check.status = 'PASS'
-                check.summary = 'Successfully deployed version %s to env %s' % (application_version_name, env)
-            else:
-                check.status = 'WARN'
-                check.summary = 'Something went wrong when provisioning: %s' \
-                                'Please check the AWS EB Console.' % deploy_succeeded
+            EBDeployer.deploy_new_version(env, repo_location, application_version_name)
+            check.status = 'PASS'
+            check.summary = 'Successfully deployed version %s to env %s' % (application_version_name, env)
         except Exception as e:
             check.status = 'ERROR'
             check.summary = 'Error encountered while deploying: %s' % str(e)
@@ -181,5 +185,5 @@ def deploy_application_to_beanstalk(connection, **kwargs):
         check.status = 'ERROR'
         check.summary = 'Could not package repository: %s' % packaging_was_successful
 
-    cleanup_tempdir(os.path.join(repo_location, '../'))  # cleanup tempdir that houses repo
+    cleanup_tempdir(repo_location)
     return check
