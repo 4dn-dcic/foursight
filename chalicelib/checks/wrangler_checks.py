b1448aa3b25ae383fc845994b66e902cf0963d79
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2021-03-18 16:54:10 -0400

    fix sheet name

--- chalicelib/checks/wrangler_checks.py
+++ chalicelib/checks/wrangler_checks.py
@@ -2290,7 +2290,7 @@ def get_oh_google_sheet():
     gc = gspread.authorize(creds)
     # Get the google sheet information
     book_id = '1zPfPjm1-QT8XdYtE2CSRA83KOhHfiRWX6rRl8E1ARSw'
-    sheet_name = 'AllMembers_Final_Test'
+    sheet_name = 'AllMembers'
     book = gc.open_by_key(book_id)
     worksheet = book.worksheet(sheet_name)
     return worksheet
