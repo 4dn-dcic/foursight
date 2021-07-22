06b69b868bffe4bb2c86f92ff3e89e9def512b29
SooLee <duplexa@gmail.com>
2021-07-22 10:02:01 -0400

    more logging

--- chalicelib/checks/helpers/wfr_utils.py
+++ chalicelib/checks/helpers/wfr_utils.py
@@ -1343,7 +1343,7 @@ def check_margi(res, my_auth, tag, check, start, lambda_limit, nore=False, nonor
                     missing_run.append(['step3', ['imargi-processing-pairs', refs['organism'], {}],
                                         inp_f, set_acc])
             else:
-                problematic_run.append(['step3-not_unique', set_acc])
+                problematic_run.append(['step3-not_unique', set_acc, all_step3s])
                 set_summary += "| problem in step 3- not unique"
         check.brief_output.append(set_summary)
         if running:
