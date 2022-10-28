f9693b1334b35ef9f59dedffec97ce14f3567974
David Michaels <david_michaels@hms.harvard.edu>
2022-10-28 13:38:42 -0400

    Experiment with renaming chalicelib to chalicelib_fourfront.

--- chalicelib_fourfront/vars.py
+++ chalicelib_fourfront/vars.py
@@ -1,3 +1,6 @@
+from os.path import dirname, join
+
 FOURSIGHT_PREFIX = 'foursight'
 DEV_ENV = 'mastertest'
 HOST = 'https://search-foursight-fourfront-ylxn33a5qytswm63z52uytgkm4.us-east-1.es.amazonaws.com'
+CHECK_SETUP_FILE = join(dirname(__file__), "check_setup.json")
