fede9758b17255408f1a4d60f81a5d3e26d351a8
David Michaels <david_michaels@hms.harvard.edu>
2022-10-29 21:45:52 -0400

    Experiment with renaming chalicelib to chalicelib_fourfront.

--- chalicelib_fourfront/app_utils.py
+++ chalicelib_fourfront/app_utils.py
@@ -1,4 +1,3 @@
-from chalice import Cron
 import os
 from os.path import dirname
 from dcicutils.env_utils import public_url_for_app
@@ -8,7 +7,8 @@ from foursight_core.identity import apply_identity_globally
 from .vars import FOURSIGHT_PREFIX, HOST
 
 
-class AppUtils(AppUtils_from_core):
+#TODO: Rename back to AppUtils with merged with 4dn-cloud-infra version below after verified working ...
+class AppUtils_from_cgap_or_fourfront(AppUtils_from_core):
 
     # dmichaels/C4-826: Apply identity globally.
     apply_identity_globally()
@@ -32,3 +32,41 @@ class AppUtils(AppUtils_from_core):
     check_setup_dir_fallback = dirname(__file__)
     DEFAULT_ENV = os.environ.get("ENV_NAME", "foursight-fourfront-env-uninitialized")
     html_main_title = 'Foursight'
+
+# From 4dn-cloud-infra - TODO: merge with above after verify working ...
+
+from dcicutils.misc_utils import remove_suffix
+
+STAGE = os.environ.get('chalice_stage', 'dev')
+HOST = os.environ.get('ES_HOST', None)
+
+FOURSIGHT_PREFIX = os.environ.get('FOURSIGHT_PREFIX')
+if not FOURSIGHT_PREFIX:
+    _GLOBAL_ENV_BUCKET = os.environ.get('GLOBAL_ENV_BUCKET') or os.environ.get('GLOBAL_BUCKET_ENV')
+    if _GLOBAL_ENV_BUCKET is not None:
+        print('_GLOBAL_ENV_BUCKET=', _GLOBAL_ENV_BUCKET)  # TODO: Temporary print statement, for debugging
+        FOURSIGHT_PREFIX = remove_suffix('-envs', _GLOBAL_ENV_BUCKET, required=True)
+        print(f'Inferred FOURSIGHT_PREFIX={FOURSIGHT_PREFIX}')
+    else:
+        raise RuntimeError('The FOURSIGHT_PREFIX environment variable is not set. Heuristics failed.')
+
+
+# This object usually in chalicelib_fourfront/app_utils.py
+class AppUtils(AppUtils_from_cgap_or_fourfront):
+    # overwriting parent class
+    prefix = FOURSIGHT_PREFIX
+    FAVICON = 'https://cgap-dbmi.hms.harvard.edu/static/img/favicon-fs.ico'
+    host = HOST
+    if IS_FOURSIGHT_FOURFRONT():
+        package_name = 'chalicelib_fourfront'
+    else:
+        package_name = 'chalicelib_cgap'
+    # check_setup is moved to vendor/ where it will be automatically placed at top level
+    check_setup_dir = os.path.dirname(__file__)
+    # html_main_title = f'Foursight-{DEFAULT_ENV}-{STAGE}'.title()
+    # html_main_title = 'Foursight-Fourfront';
+    html_main_title = "Foursight" # Foursight CGAP vs Fourfront difference now conveyed in the upper left icon.
+    DEFAULT_ENV = os.environ.get('ENV_NAME', 'foursight-fourfront-env-uninitialized')
+
+
+app_utils_obj = AppUtils.singleton(AppUtils)
