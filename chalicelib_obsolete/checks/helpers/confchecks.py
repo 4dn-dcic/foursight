aba7437b1a0f001ac0f25098b9ba938db02d7e50
David Michaels <david_michaels@hms.harvard.edu>
2022-11-02 15:33:34 -0400

    Removed chalicelib_obsolete; accidentally checked in.

--- chalicelib_obsolete/checks/helpers/confchecks.py
+++
@@ -1,7 +0,0 @@
-from foursight_core.decorators import Decorators
-from ...vars import FOURSIGHT_PREFIX
-deco = Decorators(FOURSIGHT_PREFIX)
-CheckResult = deco.CheckResult
-ActionResult = deco.ActionResult
-check_function = deco.check_function
-action_function = deco.action_function
