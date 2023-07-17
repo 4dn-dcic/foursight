49ac40545bef04b41424fda558b287bf95665474
David Michaels <david_michaels@hms.harvard.edu>
2023-07-17 10:47:41 -0400

    Adding support for local-check-execution utility.

--- chalicelib_fourfront/scripts/local_check_execution.py
+++ chalicelib_fourfront/scripts/local_check_execution.py
@@ -1,9 +1,9 @@
-from foursight_core.scripts.local_check_runner import local_check_runner
+from foursight_core.scripts.local_check_execution import local_check_execution
 from chalicelib_fourfront.app_utils import app_utils_obj as app_utils
 
 
 def main():
-    local_check_runner(app_utils)
+    local_check_execution(app_utils)
 
 
 if __name__ == "__main__":
